82253e6533d9361ec163b710c13c112d18f357c1
Денис <laiensicet@gmail.com>
2024-02-26 22:12:47 +0500

    Add files via upload

--- hifrovanie_2.py
+++ hifrovanie_2.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 
 a = ["ф", "б", "f", "g", "h",  "m", "n", "!", "в", "г", "9", "а", "е", "р", "4", "2", "ё", "и", "6", "-", "о",
      "у", "д", "ж", "з", "й", "к", "t", "z", "r", "л", "?", "i", "j", "k", "l", "7", "м", "3", "н", "v", "p", "w",
@@ -12,150 +13,58 @@ class Ui_MainWindow(object):
         MainWindow.resize(1040, 760)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        font_1, font_2, font_3, font_4, font_5, font_6 = QtGui.QFont(), QtGui.QFont(), QtGui.QFont(), QtGui.QFont(),\
+            QtGui.QFont(), QtGui.QFont()
+        self.spisok_font = [[font_1, 12, False, 50, "Segoe UI"], [font_2, 16, True, 75, "Segoe Print"],
+                            [font_3, 14, True, 75, "Segoe Print"], [font_4, 12, False, 50, "Segoe Print"],
+                            [font_5, 7, True, 75, "Segoe Print"], [font_6, 12, True, 75, "Segoe Print"]]
+        for i in self.spisok_font:
+            i[0].setFamily(i[4])
+            i[0].setPointSize(i[1])
+            i[0].setBold(i[2])
+            i[0].setWeight(i[3])
+
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(20, 30, 1000, 250))
-        font = QtGui.QFont()
-        font.setFamily("Segoe UI")
-        font.setPointSize(12)
-        self.textEdit.setFont(font)
-        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setFont(font_1)
+
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(20, 460, 1000, 250))
-        font = QtGui.QFont()
-        font.setFamily("Segoe UI")
-        font.setPointSize(12)
-        self.textBrowser.setFont(font)
-        self.textBrowser.setObjectName("textBrowser")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(520, 300, 160, 25))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(530, 330, 30, 30))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(590, 330, 30, 30))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(650, 330, 30, 30))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
-        self.spinBox = QtWidgets.QSpinBox(self.centralwidget, value=1)
-        self.spinBox.setGeometry(QtCore.QRect(520, 370, 35, 35))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.spinBox.setFont(font)
-        self.spinBox.setRange(1, 9)
-        self.spinBox.setObjectName("spinBox")
-        self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(570, 420, 100, 25))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(12)
-        font.setBold(False)
-        font.setWeight(50)
-        self.label_5.setFont(font)
-        self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_6.setGeometry(QtCore.QRect(820, 300, 160, 70))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_6.setFont(font)
-        self.label_6.setObjectName("label_6")
-        self.label_7 = QtWidgets.QLabel(self.centralwidget)
-        self.label_7.setGeometry(QtCore.QRect(880, 420, 140, 25))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(7)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_7.setFont(font)
-        self.label_7.setObjectName("label_7")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(170, 310, 200, 50))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton.setFont(font)
+        self.textBrowser.setFont(font_1)
 
-        self.pushButton.setObjectName("pushButton")
-        self.label_8 = QtWidgets.QLabel(self.centralwidget)
-        self.label_8.setGeometry(QtCore.QRect(30, 390, 450, 55))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(12)
-        font.setBold(False)
-        font.setWeight(50)
-        self.label_8.setFont(font)
-        self.label_8.setObjectName("label_8")
+        self.label, self.label_2, self.label_3, self.label_4, self.label_5, self.label_6, self.label_7, self.label_8,\
+            self.label_9 = QtWidgets.QLabel(self.centralwidget), QtWidgets.QLabel(self.centralwidget),\
+            QtWidgets.QLabel(self.centralwidget), QtWidgets.QLabel(self.centralwidget),\
+            QtWidgets.QLabel(self.centralwidget), QtWidgets.QLabel(self.centralwidget),\
+            QtWidgets.QLabel(self.centralwidget), QtWidgets.QLabel(self.centralwidget),\
+            QtWidgets.QLabel(self.centralwidget)
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+
+        self.spisok_l = [[self.label, 520, 300, 160, 25, font_2, 'доп. «ключи»:'],
+                         [self.label_2, 530, 330, 30, 30, font_2, 'x'], [self.label_3, 590, 330, 30, 30, font_2, 'y'],
+                         [self.label_4, 650, 330, 30, 30, font_2, 'z'], [self.label_5, 550, 415, 100, 25, font_4, '(от 1 до 9)'],
+                         [self.label_6, 820, 300, 160, 70, font_2, 'главный \nотступ: '],
+                         [self.label_7, 880, 420, 140, 25, font_5, 'разработчик: Тарасов Д.Л.'],
+                         [self.label_8, 30, 390, 450, 55, font_4, ''], [self.label_9, 20, 0, 60, 25, font_6, 'сюда:'],
+                         [self.pushButton, 170, 310, 200, 50, font_2, "делать. "]]
+        for i in self.spisok_l:
+            i[0].setGeometry(QtCore.QRect(i[1], i[2], i[3], i[4]))
+            i[0].setFont(i[5])
+
+        self.spinBox = QtWidgets.QSpinBox(self.centralwidget, value=1)
         self.spinBox_2 = QtWidgets.QSpinBox(self.centralwidget, value=2)
-        self.spinBox_2.setGeometry(QtCore.QRect(580, 370, 35, 35))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.spinBox_2.setFont(font)
-        self.spinBox_2.setRange(1, 9)
-        self.spinBox_2.setObjectName("spinBox_2")
         self.spinBox_3 = QtWidgets.QSpinBox(self.centralwidget, value=3)
-        self.spinBox_3.setGeometry(QtCore.QRect(640, 370, 35, 35))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.spinBox_3.setFont(font)
-        self.spinBox_3.setRange(1, 9)
-        self.spinBox_3.setObjectName("spinBox_3")
         self.spinBox_4 = QtWidgets.QSpinBox(self.centralwidget)
-        self.spinBox_4.setGeometry(QtCore.QRect(930, 340, 60, 35))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.spinBox_4.setFont(font)
-        self.spinBox_4.setObjectName("spinBox_4")
-        self.label_9 = QtWidgets.QLabel(self.centralwidget)
-        self.label_9.setGeometry(QtCore.QRect(20, 0, 60, 25))
-        font = QtGui.QFont()
-        font.setFamily("Segoe Print")
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_9.setFont(font)
-        self.label_9.setObjectName("label_9")
+
+        self.spisok_spinB = [[self.spinBox, 520, 370, 35, 1, 9], [self.spinBox_2, 580, 370, 35, 1, 9],\
+                             [self.spinBox_3, 640, 370, 35, 1, 9], [self.spinBox_4, 930, 340, 60, 0, 25]]
+        for i in self.spisok_spinB:
+            i[0].setGeometry(QtCore.QRect(i[1], i[2], i[3], 35))
+            i[0].setFont(font_3)
+            i[0].setRange(i[4], i[5])
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1040, 20))
@@ -171,17 +80,9 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "шифр."))
-        self.label.setText(_translate("MainWindow", "доп. «ключи»:"))
-        self.label_2.setText(_translate("MainWindow", "x"))
-        self.label_3.setText(_translate("MainWindow", "y"))
-        self.label_4.setText(_translate("MainWindow", "z"))
-        self.label_5.setText(_translate("MainWindow", "(от 1 до 9)"))
-        self.label_6.setText(_translate("MainWindow", "главный \nотступ: "))
-        self.label_7.setText(_translate("MainWindow", "разработчик: Тарасов Д.Л."))
-        self.pushButton.setText(_translate("MainWindow", "делать. "))
-        self.label_8.setText(_translate("MainWindow", "*тех. строка*****************************************"
-                                                      "\n*******************************************************"))
-        self.label_9.setText(_translate("MainWindow", "сюда:"))
+        for i in self.spisok_l:
+            i[0].setText(_translate("MainWindow", i[6]))
+
 
 
 def knopka():
@@ -312,14 +213,11 @@ def de_hifr():
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-
-    ui.label_8.setText("")
+    ui.label_8.setText('')
     ui.pushButton.clicked.connect(knopka)
-
     sys.exit(app.exec_())
